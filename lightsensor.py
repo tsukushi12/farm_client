c630631da1f8d41e69b8953dc9fdb1ea6d572863
tsukushi12 <tsukushi129@gmail.com>
2017-09-20 04:05:12 +0000

    light sensor

---
+++ lightsensor.py
@@ -0,0 +1,13 @@
+import grovepi
+import time
+
+light_sensor = 0
+grovepi.pinMode(light_sensor, "INPUT")
+
+def getdata():
+	sensor_value = grovepi.analogRead(light_sensor)
+	return sensor_value
+
+if __name__ == "__main__":
+	time.sleep(3)
+	print getdata()
